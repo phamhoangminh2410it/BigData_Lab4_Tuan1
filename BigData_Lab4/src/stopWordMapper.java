abcade2f9ae3a008ed08a78da947ac2df442242d
Minh <125272514+phamhoangminh2410it@users.noreply.github.com>
2023-11-16 11:04:04 +0700

    Add files via upload

---
+++ BigData_Lab4/src/stopWordMapper.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author phamh
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.Iterator;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class stopWordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private Set StopWords = new HashSet();
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        readFile(stopWords.stopWordsFiles);
+    }
+/**
+* map function of Mapper parent class takes a line of text at a
+time splits to
+* tokens and passes to the context as word along with value as
+one
+*/
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            if (StopWords.contains(token)) {
+                context.getCounter(stopWords.COUNTERS.STOPWORDS).increment(1L);
+            } else {
+                context.getCounter(stopWords.COUNTERS.GOODWORDS).increment(1L);
+                word.set(token);
+                context.write(word, null);
+            }
+        }
+    }
+    private void readFile(Path filePath) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath.toString()));
+            String stopWord = null;
+            while ((stopWord = bufferedReader.readLine()) != null) {
+                StopWords.add(stopWord.toLowerCase());
+            }
+            bufferedReader.close();
+        } catch (IOException ex) {
+            System.err.println("Exception while reading stop words file: " + ex.getMessage());
+        }
+    }
+}
